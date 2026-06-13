f847aff95b6738aefe2782f5c8c39dad560e231f
shaynewanjiru <shayne.wanjiru@student.moringaschool.com>
2026-06-13 21:52:20 +0300

    Add new songs feature

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,50 @@
-class Song:
-    pass
+class Song():
+    count = 0 
+    genres = []
+    artist = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.add_song_to_count()
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+        Song.add_to_genre_count(self.genre)
+        Song.add_to_artists_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genre:
+            cls.genres.append(genre)
+            cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artist:
+            cls.genres.append(artist)
+            cls.genre_count[artist] = cls.artist_count.get(artist, 0) + 1
+     
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1   
+
+Song = {"Rap": 5, "Rock": 1, "Country": 3}  
+Song = {"Beyonce": 17, "Jay-Z": 40}          
